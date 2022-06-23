2b5ed8214d34833a1db59eeadae1e1f2234881c2
Pedro Vinicius Monteiro <73789587+PedroViniciusMonteiro@users.noreply.github.com>
2022-06-23 18:13:34 -0300

    Protocolos de Comunicação

--- Introducao/index.php
+++ Introducao/index.php
@@ -1,8 +1,11 @@
 <!-- 
+
 Objetivo Geral -
 APIs, servidores, clientes se comunicam na internet através de um protocolo de comunicação.
 Este protocolo de comunicação determina o comportamento das partes.
 Esse cenário pode influenciar no desenvolvimento da aplicação.
 Sendo assim, é preciso conhecer minimamente os cenários existentes a fim de mitigar possíveis erros.
+
+HTTP / HTTPS / WebSocket-APIs
 
 -->
